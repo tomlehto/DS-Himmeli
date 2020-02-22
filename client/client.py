269508f8d2df5636150c0187db1c8423b891da9e
Bekim <bekim.abazi95@gmail.com>
2020-02-22 16:24:29 +0200

    Add command line arguments

--- client/client.py
+++ client/client.py
@@ -1,6 +1,12 @@
 import paho.mqtt.client as mqtt
 import random
 import time
+import sys
+
+if (3 != len(sys.argv)):
+    print("Wrong number of arguments!")
+    print("Usage: python [file] [ip/address] [client_name]")
+    sys.exit(0)
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -14,16 +20,14 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
 
-
 def read_sensor():
     return random.randint(-30,30)
-
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("127.0.0.1", 1883, 60)
+client.connect(str(sys.argv[1]), 1883, 60)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
@@ -36,5 +40,5 @@ while True:
     time.sleep(1)
     # TODO: change read_sensor call to stubbed Sensor class method
     temperature = read_sensor()
-    client.publish("test/temperature", temperature)
+    client.publish("sensors/"+str(sys.argv[2])+"/temperature", temperature)
 
